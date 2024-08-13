72f24d66bed5399c7e8d109fddaf65c2cfb2ea14
David Egbuwalo <lereegbuwalo@gmail.com>
2024-08-13 19:12:09 +0100

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#060B26",
+        type_gray: "#A0AEC0",
+        bg_gray: "#1A1F37",
+        pri_blue: "#0075FF",
+        pri_green: "#01B574",
+        pri_red: "#E31A1A",
+        slight_dark: "#2D2E5F",
+      },
+    },
+  },
+  plugins: [],
+};
