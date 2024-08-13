c18ec2841f1d5f4e3074006448a8b9620fbdb8ae
David Egbuwalo <lereegbuwalo@gmail.com>
2024-08-13 19:58:21 +0100

    feat: add routes for landing page and page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'landing',
+    loadChildren: () =>
+      import('./features/landing-page/landing-page.route').then(
+        (m) => m.landingPageRoute,
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/page-not-found/page-not-found.component').then(
+        (c) => c.PageNotFoundComponent,
+      ),
+  },
+];
---
+++ src/app/features/landing-page/landing-page.route.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page.component';
+
+export const landingPageRoute: Routes = [
+  { path: '', component: LandingPageComponent },
+];
