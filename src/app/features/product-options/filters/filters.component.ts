4a090f1021dbc8c946639d7a976b004c11a874b5
David Egbuwalo <lereegbuwalo@gmail.com>
2024-09-05 23:00:42 +0100

    feat: add prime ng

--- src/app/features/product-options/filters/filters.component.ts
+++ src/app/features/product-options/filters/filters.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { SliderModule } from 'primeng/slider';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [NgClass],
+  imports: [
+    NgClass,
+    SliderModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    FormsModule,
+  ],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss',
 })
 export class FiltersComponent implements OnInit {
   isShowing: boolean[] = [true];
+  // rangeValues = new FormControl([20, 30]);
+  rangeValues = [20, 30];
 
   ngOnInit() {
     console.log(this.isShowing);
+    // this.rangeValues.valueChanges.subscribe((val) => {
+    //   console.log(val);
+    // });
   }
 
   onToggleFilter(index: number) {
