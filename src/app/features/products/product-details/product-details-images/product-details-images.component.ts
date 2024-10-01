61aa9e3fca6ae7e28d6339f47692606624081916
David Egbuwalo <lereegbuwalo@gmail.com>
2024-10-01 14:55:52 +0100

    refactor: galleria style

--- src/app/features/products/product-details/product-details-images/product-details-images.component.ts
+++ src/app/features/products/product-details/product-details-images/product-details-images.component.ts
@@ -20,19 +20,19 @@ export class ProductDetailsImagesComponent implements OnInit {
   ];
   responsiveOptions: any[] | undefined;
   ngOnInit() {
-    // this.responsiveOptions = [
-    //   {
-    //     breakpoint: '1024px',
-    //     numVisible: 5,
-    //   },
-    //   {
-    //     breakpoint: '768px',
-    //     numVisible: 3,
-    //   },
-    //   {
-    //     breakpoint: '560px',
-    //     numVisible: 1,
-    //   },
-    // ];
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 5,
+      },
+      {
+        breakpoint: '850px',
+        numVisible: 3,
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+      },
+    ];
   }
 }
