cba9ec61741ef03032b31c50e5beb84ad82bd6b4
David Egbuwalo <lereegbuwalo@gmail.com>
2024-10-05 19:51:25 +0100

    feat: login menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { LayoutService } from '../../../shared/services/layout.service';
 import { RouterLink } from '@angular/router';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgClass, RouterLink],
+  imports: [NgClass, RouterLink, MenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
