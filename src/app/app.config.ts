9aac967d2da3058480f6943ec1b75c3a7510117a
David Egbuwalo <lereegbuwalo@gmail.com>
2024-10-09 15:47:46 +0100

    feat: add scroll to top on route change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, inMemoryScrollingFeature),
     importProvidersFrom([BrowserAnimationsModule]),
   ],
 };
