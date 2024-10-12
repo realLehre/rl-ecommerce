9b57f7874a9697ccb7542d43d00be6813dd5f981
David Egbuwalo <lereegbuwalo@gmail.com>
2024-10-12 13:03:47 +0100

    refactor: sign up form

--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -6,19 +11,22 @@ import {
   Validators,
 } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ErrorMessageDirective } from '../../user/address/address-form/directives/error-message.directive';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, ErrorMessageDirective, NgClass],
   templateUrl: './sign-up.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
   fb = inject(FormBuilder);
   signupForm!: FormGroup;
   showPassword: boolean[] = [];
+  passwordMatch: boolean = true;
 
   constructor() {}
 
@@ -39,6 +47,19 @@ export class SignUpComponent {
     }
   }
 
+  onKey(event: any) {
+    if (!this.signupForm.get('confirmPassword')?.dirty) {
+      return;
+    }
+    this.passwordMatch =
+      this.signupForm.get('password')?.value ===
+      this.signupForm.get('confirmPassword')?.value;
+  }
+
+  isInvalidAndTouched(controlName: string): boolean {
+    const control = this.signupForm.get(controlName);
+    return control ? control.invalid && control.touched : false;
+  }
   onTogglePasswordVisibility(index: number) {
     this.showPassword[index] = !this.showPassword[index];
   }
