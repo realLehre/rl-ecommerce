536ac5d68a0ad3314ed8e9020a653360f9246bd9
David Egbuwalo <lereegbuwalo@gmail.com>
2024-11-08 21:05:13 +0100

    feat: add live api link

--- apps/rl-ecommerce/src/environments/environment.ts
+++ apps/rl-ecommerce/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   supabaseUrl: 'https://tentdyesixetvyacewwr.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbnRkeWVzaXhldHZ5YWNld3dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM1MzQ3NDgsImV4cCI6MjAzOTExMDc0OH0.jb1sO6U5cEn-GIuEA-OsLTV8e4EDcN_BrA9EdcII0qA',
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://rl-ecommerce-api.onrender.com/api',
   paystackKey: 'sk_test_32ac4a7a61f44b662aa4d522ead086f0b13d31b6',
   publicPaystackKey: 'pk_test_b95a88357b0b5b7c4e20ccceb1ac928bb2f9a5bf',
 };
