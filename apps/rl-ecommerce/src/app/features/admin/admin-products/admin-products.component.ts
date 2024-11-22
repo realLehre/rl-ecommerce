6b09e8fb69fdb79145f4a5c48b180c1c6519f3c6
David Egbuwalo <lereegbuwalo@gmail.com>
2024-11-22 22:10:25 +0100

    add pagination to product table

--- apps/rl-ecommerce/src/app/features/admin/admin-products/admin-products.component.ts
+++ apps/rl-ecommerce/src/app/features/admin/admin-products/admin-products.component.ts
@@ -1,12 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { AdminProductsService } from './services/admin-products.service';
 import { GenericTableComponent } from '../../../shared/components/generic-table/generic-table.component';
-import { CurrencyPipe, DatePipe, NgClass } from '@angular/common';
+import { CurrencyPipe, DatePipe, DecimalPipe, NgClass } from '@angular/common';
 import { IOrder } from '../../../shared/models/order.interface';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ICategory, IProduct } from '../../products/model/product.interface';
-import { MenuModule } from 'primeng/menu';
+import { Menu, MenuModule } from 'primeng/menu';
 import { PrimeTemplate } from 'primeng/api';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { SliderModule } from 'primeng/slider';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-products',
@@ -18,20 +22,88 @@ import { PrimeTemplate } from 'primeng/api';
     DatePipe,
     MenuModule,
     PrimeTemplate,
+    CalendarModule,
+    DecimalPipe,
+    DropdownModule,
+    SliderModule,
+    FormsModule,
   ],
   templateUrl: './admin-products.component.html',
   styleUrl: './admin-products.component.scss',
 })
 export class AdminProductsComponent {
   private productService = inject(AdminProductsService);
-  products = toSignal(this.productService.products, { initialValue: [] });
+  products = toSignal(this.productService.productsData, {
+    initialValue: { totalPages: 1, products: [] },
+  });
   sortUsed: boolean = false;
   sortColumn: keyof IProduct | keyof ICategory | '' = '';
   sortDirection: 'asc' | 'desc' = 'asc';
+  @ViewChild('menu') menu!: Menu;
+  filterNumber = 0;
+  rangeValues = [2000, 10000];
+  categories: ICategory[] = [];
+  selectedCategory!: ICategory;
+  itemsToShow: number[] = [1, 5, 10, 15, 20, 25];
+  totalItemsToShow: number = 10;
+  filter: {
+    minPrice?: number;
+    maxPrice?: number;
+    category?: any;
+    itemsToShow: number;
+    page?: number;
+    orderId?: string;
+    minDate?: any;
+    maxDate?: any;
+  } = {
+    itemsToShow: this.totalItemsToShow,
+    page: 1,
+  };
+  rangeDates: any[] = [];
 
   onEdit() {}
 
   onDelete() {}
+
+  onApplyFilter() {
+    // this.filterNumber = this.findFilterNumber();
+    this.menu.hide();
+  }
+
+  onDateChanged() {
+    if (this.rangeDates[0] && this.rangeDates[1]) {
+      let dates = [...this.rangeDates];
+      dates = dates.map((date) => this.productService.formatDate(date));
+      this.filter = {
+        ...this.filter,
+        page: 1,
+        minDate: dates[0],
+        maxDate: dates[1],
+      };
+    }
+  }
+
+  onChangeCategory(cat: ICategory) {
+    this.selectedCategory = cat;
+
+    this.filter = { ...this.filter, page: 1, category: cat.id };
+  }
+
+  onRangeValueChanged(value: any[]) {
+    this.filter = {
+      ...this.filter,
+      minPrice: value[0],
+      maxPrice: value[1],
+    };
+  }
+
+  onClearFilter() {
+    if (this.filterNumber == 0) {
+      return;
+    }
+    this.menu.hide();
+    this.rangeValues = [2000, 10000];
+  }
 
   sortTable(column: keyof IProduct | keyof ICategory): void {
     if (this.sortColumn === column) {
@@ -42,7 +114,7 @@ export class AdminProductsComponent {
     }
     this.sortUsed = true;
 
-    this.products().sort((a, b) => {
+    this.products()?.products.sort((a, b) => {
       // if(column == 'category'){
       //   const valueA = a[column];
       //   const valueB = b[column];
--- apps/rl-ecommerce/src/app/shared/components/generic-table/generic-table.component.ts
+++ apps/rl-ecommerce/src/app/shared/components/generic-table/generic-table.component.ts
@@ -4,12 +4,14 @@ import {
   inject,
   input,
   TemplateRef,
+  ViewChild,
 } from '@angular/core';
 import { AdminProductsService } from '../../../features/admin/admin-products/services/admin-products.service';
-import { NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { IProduct } from '../../../features/products/model/product.interface';
 import { NgxPaginationModule, PaginationInstance } from 'ngx-pagination';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-generic-table',
@@ -19,6 +21,7 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
     NgxPaginationModule,
     FormsModule,
     ReactiveFormsModule,
+    NgClass,
   ],
   templateUrl: './generic-table.component.html',
   styleUrl: './generic-table.component.scss',
@@ -27,11 +30,25 @@ export class GenericTableComponent {
   @ContentChild('headers') headers!: TemplateRef<any>;
   @ContentChild('rows') rows!: TemplateRef<any>;
   title = input.required<string>();
-  data = input.required<any[]>();
+  data = input.required<any>();
   config: PaginationInstance = {
     id: 'userOrderPagination',
     itemsPerPage: 10,
     currentPage: 1,
   };
   searchInput: FormControl = new FormControl(null);
+  itemsToShow: number[] = [1, 5, 10, 15, 20, 25];
+  totalItemsToShow: number = 10;
+
+  onChangeItemsToShow(total: number) {
+    this.totalItemsToShow = total;
+    this.config.itemsPerPage = total;
+  }
+
+  pageChange(event: any) {
+    window.scrollTo({
+      top: 70,
+      behavior: 'smooth',
+    });
+  }
 }
