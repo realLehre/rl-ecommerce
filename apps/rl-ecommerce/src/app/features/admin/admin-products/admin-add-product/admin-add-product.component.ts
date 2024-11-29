7051f9d714687820fb9e84412e7e6238c99086b7
David Egbuwalo <lereegbuwalo@gmail.com>
2024-11-29 02:40:45 +0100

    feat: init add product form

--- apps/rl-ecommerce/src/app/features/admin/admin-products/admin-add-product/admin-add-product.component.ts
+++ apps/rl-ecommerce/src/app/features/admin/admin-products/admin-add-product/admin-add-product.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { ErrorMessageDirective } from '../../../user/address/address-form/directives/error-message.directive';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { ProductOptionsService } from '../../../product-options/services/product-options.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DropdownModule } from 'primeng/dropdown';
+import {
+  ICategory,
+  ISubCategory,
+} from '../../../product-options/models/product-options.interface';
 
 @Component({
   selector: 'app-admin-add-product',
   standalone: true,
-  imports: [],
+  imports: [
+    ErrorMessageDirective,
+    ReactiveFormsModule,
+    NgClass,
+    DropdownModule,
+  ],
   templateUrl: './admin-add-product.component.html',
-  styleUrl: './admin-add-product.component.scss'
+  styleUrl: './admin-add-product.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminAddProductComponent {
+export class AdminAddProductComponent implements OnInit {
+  private optionsService = inject(ProductOptionsService);
+  private fb = inject(FormBuilder);
+  productForm!: FormGroup;
+  categories = toSignal(this.optionsService.getCategories());
+  subCategories: ISubCategory[] = [];
 
+  ngOnInit() {
+    this.productForm = this.fb.group({
+      name: [null, Validators.required],
+      price: [null, Validators.required],
+      category: [null, Validators.required],
+      subCategory: [null, Validators.required],
+    });
+
+    this.productForm
+      .get('category')
+      ?.valueChanges.subscribe((res: ICategory) => {
+        this.subCategories = res.subCategories;
+      });
+  }
+
+  isInvalidAndTouched(control: any) {
+    return (
+      this.productForm.get(control)?.touched &&
+      this.productForm.get(control)?.invalid
+    );
+  }
 }
