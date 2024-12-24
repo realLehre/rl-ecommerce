5144e3418b7644fb29993c26b9028a8b594014d5
David Egbuwalo <lereegbuwalo@gmail.com>
2024-12-24 03:30:06 +0100

    feat: disable dropdown item corresponding to the current order delivery status

--- apps/rl-ecommerce/src/app/features/admin/admin-orders/admin-order-details/admin-order-status/admin-order-status.component.ts
+++ apps/rl-ecommerce/src/app/features/admin/admin-orders/admin-order-details/admin-order-status/admin-order-status.component.ts
@@ -1,43 +1,51 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ElementRef,
+  computed,
   input,
   ViewChild,
 } from '@angular/core';
 import { IOrder } from '../../../../../shared/models/order.interface';
 import { FormsModule } from '@angular/forms';
 import { Dropdown, DropdownModule } from 'primeng/dropdown';
-import { OrderTrackerComponent } from '../../../../user/user-orders/user-order-details/order-tracker/order-tracker.component';
 import { AdminOrderTrackingEventsComponent } from '../admin-order-tracking-events/admin-order-tracking-events.component';
-import { OrderStatusText } from '../../../../user/user-orders/user-order-details/order-tracker/directives/order-status-icon.directive';
 
 @Component({
   selector: 'app-admin-order-status',
   standalone: true,
-  imports: [
-    FormsModule,
-    DropdownModule,
-    OrderTrackerComponent,
-    AdminOrderTrackingEventsComponent,
-    OrderStatusText,
-  ],
+  imports: [FormsModule, DropdownModule, AdminOrderTrackingEventsComponent],
   templateUrl: './admin-order-status.component.html',
   styleUrl: './admin-order-status.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdminOrderStatusComponent {
   order = input<IOrder>({} as IOrder);
-  deliveryStatus: { name: string; code: string }[] = [
-    { name: 'Packed', code: 'PACKED' },
-    { name: 'Delivered', code: 'DELIVERED' },
-  ];
-  selectedStatus!: { name: string; code: string };
+  deliveryStatus = computed<
+    { name: string; code: string; inactive: boolean }[]
+  >(() => {
+    return [
+      {
+        name: 'Packed',
+        code: 'PACKED',
+        inactive:
+          this.order().deliveryStatus?.toLowerCase() === 'packed' ||
+          this.order().deliveryStatus?.toLowerCase() === 'delivered',
+      },
+      {
+        name: 'Delivered',
+        code: 'DELIVERED',
+        inactive: this.order().deliveryStatus?.toLowerCase() === 'delivered',
+      },
+    ];
+  });
+  selectedStatus!: { name: string; code: string; inactive: boolean };
   @ViewChild('dropDown') dropDown!: Dropdown;
 
   onToggleDropDown() {
     this.dropDown.show();
   }
 
-  onChangeOrderStatus(event: any) {}
+  onChangeOrderStatus(event: any) {
+    console.log(event);
+  }
 }
