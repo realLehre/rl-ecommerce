4b868a8d05d7a83bf7804d15058397a92ba402e6
David Egbuwalo <lereegbuwalo@gmail.com>
2024-12-27 02:24:49 +0100

    fix: product quantity add bug

--- apps/rl-ecommerce/src/app/shared/components/product-quantity/product-quantity.component.ts
+++ apps/rl-ecommerce/src/app/shared/components/product-quantity/product-quantity.component.ts
@@ -22,12 +22,12 @@ export class ProductQuantityComponent implements OnInit {
   private toast = inject(ToastService);
   quantity = input.required<number>();
   productUnitsLeft = input.required<number>();
-  quantityMain!: number;
+  quantityMain = signal(1);
   quantityChanged = output<number>();
   isLoading = input<boolean>(false);
 
   ngOnInit() {
-    this.quantityMain = this.quantity();
+    this.quantityMain.set(this.quantity());
   }
 
   onAdjustQuantity(action: string) {
@@ -35,20 +35,20 @@ export class ProductQuantityComponent implements OnInit {
       return;
     }
     if (action.toLowerCase() == 'increase') {
-      if (this.quantityMain == this.productUnitsLeft()) {
+      if (this.quantityMain() == this.productUnitsLeft()) {
         this.toast.showToast({
           type: 'error',
           message: 'Order quantity can not exceed available units!',
         });
         return;
       }
-      this.quantityMain++;
+      this.quantityMain.update((count) => count + 1);
     } else {
-      if (this.quantity() == 1) {
+      if (this.quantityMain() == 1) {
         return;
       }
-      this.quantityMain--;
+      this.quantityMain.update((count) => count - 1);
     }
-    this.quantityChanged.emit(this.quantityMain);
+    this.quantityChanged.emit(this.quantityMain());
   }
 }
