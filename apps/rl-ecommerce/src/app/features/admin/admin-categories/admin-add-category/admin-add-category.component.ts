126edca821f71d69fe5e0845147f40b1dbf8a4cb
David Egbuwalo <lereegbuwalo@gmail.com>
2024-12-28 10:39:17 +0100

    add category form

--- apps/rl-ecommerce/src/app/features/admin/admin-categories/admin-add-category/admin-add-category.component.ts
+++ apps/rl-ecommerce/src/app/features/admin/admin-categories/admin-add-category/admin-add-category.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { AdminCategoriesService } from '../services/admin-categories.service';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ErrorMessageDirective } from '../../../user/address/address-form/directives/error-message.directive';
+import { NgClass } from '@angular/common';
+import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-admin-add-category',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    ErrorMessageDirective,
+    NgClass,
+    LoaderComponent,
+  ],
   templateUrl: './admin-add-category.component.html',
-  styleUrl: './admin-add-category.component.scss'
+  styleUrl: './admin-add-category.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminAddCategoryComponent {
+export class AdminAddCategoryComponent implements OnInit {
+  private readonly categoryService = inject(AdminCategoriesService);
+  private fb = inject(FormBuilder);
+  isEditing = signal(false);
+  categoryForm!: FormGroup;
+  isSubmitting = signal(false);
 
+  ngOnInit(): void {
+    this.categoryForm = this.fb.group({
+      categoryName: [null, Validators.required],
+      subCategories: this.fb.array([
+        this.fb.group({
+          subCategoryName: [null],
+        }),
+      ]),
+    });
+  }
+
+  get subCategories() {
+    return <FormArray>this.categoryForm.get('subCategories');
+  }
+
+  onSubmit() {
+    console.log(this.categoryForm.value);
+  }
+
+  isInvalidAndTouched(control: string) {
+    return (
+      this.categoryForm.get(control)?.touched &&
+      this.categoryForm.get(control)?.invalid
+    );
+  }
+
+  onAddNewSubCategoryInput() {
+    this.subCategories.push(
+      this.fb.group({
+        subCategoryName: [null],
+      }),
+    );
+  }
+
+  onRemoveControl(i: number) {
+    this.subCategories.removeAt(i);
+  }
 }
