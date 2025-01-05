585d31f2781e6937fd431bb5fd5bf18337de9cce
David Egbuwalo <lereegbuwalo@gmail.com>
2025-01-05 18:54:48 +0100

    refactor: remove manual address subscription in checkout address

--- apps/rl-ecommerce/src/app/features/checkout/checkout-address/checkout-address.component.ts
+++ apps/rl-ecommerce/src/app/features/checkout/checkout-address/checkout-address.component.ts
@@ -2,7 +2,6 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
-  OnInit,
   output,
   signal,
 } from '@angular/core';
@@ -11,9 +10,9 @@ import { IAddress } from '../../user/models/address.interface';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { RouterLink } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
 import { SkeletonModule } from 'primeng/skeleton';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-checkout-address',
@@ -23,28 +22,26 @@ import { toSignal } from '@angular/core/rxjs-interop';
     ReactiveFormsModule,
     RadioButtonModule,
     RouterLink,
-    AsyncPipe,
     SkeletonModule,
   ],
   templateUrl: './checkout-address.component.html',
   styleUrl: './checkout-address.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckoutAddressComponent implements OnInit {
+export class CheckoutAddressComponent {
   private addressService = inject(AddressService);
-  addresses!: IAddress[];
-  selectedAddress!: IAddress;
   isLoading = signal(true);
+  addresses = toSignal(
+    this.addressService.getAddress().pipe(
+      tap((res) => {
+        this.isLoading.set(false);
+        this.selectedAddress = res.find((address) => address.isDefault)!;
+        this.selectedAddressEmit.emit(this.selectedAddress);
+      }),
+    ),
+  );
+  selectedAddress!: IAddress;
   selectedAddressEmit = output<IAddress>();
-
-  ngOnInit() {
-    this.addressService.getAddress().subscribe((res) => {
-      this.isLoading.set(false);
-      this.addresses = res;
-      this.selectedAddress = res.find((address) => address.isDefault)!;
-      this.selectedAddressEmit.emit(this.selectedAddress);
-    });
-  }
 
   onEditAddress(address: IAddress) {
     this.addressService.activeAddress.set(address);
