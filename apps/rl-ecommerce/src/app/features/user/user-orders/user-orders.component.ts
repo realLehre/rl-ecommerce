9b13157b4efba23e5d484730e9a9f61d6b86c38f
David Egbuwalo <lereegbuwalo@gmail.com>
2025-01-07 12:26:51 +0100

    feat: add no-orders message when user has placed no orders

--- apps/rl-ecommerce/src/app/features/user/user-orders/user-orders.component.ts
+++ apps/rl-ecommerce/src/app/features/user/user-orders/user-orders.component.ts
@@ -1,19 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BreadcrumbComponent } from '../../../shared/components/breadcrumb/breadcrumb.component';
 import { UserOrdersTableComponent } from './user-orders-table/user-orders-table.component';
-import { OrderService } from '../../../shared/services/order.service';
-import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-orders',
   standalone: true,
-  imports: [BreadcrumbComponent, UserOrdersTableComponent, AsyncPipe],
+  imports: [BreadcrumbComponent, UserOrdersTableComponent],
   templateUrl: './user-orders.component.html',
   styleUrl: './user-orders.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserOrdersComponent {
-  private orderService = inject(OrderService);
-  // orders: Observable<any> = this.orderService.getOrder();
-}
+export class UserOrdersComponent {}
